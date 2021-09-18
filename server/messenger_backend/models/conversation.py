2abe6c201ea53341fe7b3751bbbfc42497c9864b
biancahpp <bianca@DESKTOP-V1AQPSR.localdomain>
2021-09-18 17:04:46 -0600

    feat: conversation model updated

--- server/messenger_backend/models/conversation.py
+++ server/messenger_backend/models/conversation.py
@@ -1,28 +1,23 @@
 from django.db import models
-from django.db.models import Q
+from django.db.models import Q, Count
 
 from . import utils
 from .user import User
 
 
 class Conversation(utils.CustomModel):
-
-    user1 = models.ForeignKey(
-        User, on_delete=models.CASCADE, db_column="user1Id", related_name="+"
-    )
-    user2 = models.ForeignKey(
-        User, on_delete=models.CASCADE, db_column="user2Id", related_name="+", 
+    # a conversation can have many users
+    senders = models.ManyToManyField(
+        User, on_delete=models.CASCADE, related_name='conversations'
     )
     createdAt = models.DateTimeField(auto_now_add=True, db_index=True)
     updatedAt = models.DateTimeField(auto_now=True)
 
-    # find conversation given two user Ids
-    def find_conversation(user1Id, user2Id):
+    # find conversation given all senders ids
+    def find_conversation(sendersIds):
         # return conversation or None if it doesn't exist
         try:
-            return Conversation.objects.get(
-                (Q(user1__id=user1Id) | Q(user1__id=user2Id)),
-                (Q(user2__id=user1Id) | Q(user2__id=user2Id)),
-            )
+            return Conversation.objects.annotate(num_correct_ids=Count('ids', filter=Q(ids__in=sendersIds))).filter(num_correct_ids=len(sendersIds))
+
         except Conversation.DoesNotExist:
             return None
